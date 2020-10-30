f79992575171860e0142a9d40142337da08570aa
Hans Kokx <hans.d.kokx@gmail.com>
2020-10-29 23:27:37 -0400

    Basic API now works
    Signed-off-by: Hans Kokx <hans.d.kokx@gmail.com>

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -14,73 +14,87 @@ main.use(bodyParser.json());
 main.use(bodyParser.urlencoded({ extended: false }));
 main.use("/api/v1", app);
 
-const contactsCollection = "contacts";
+const postsCollection = "posts";
 
 export const webApi = functions.https.onRequest(main);
 
-interface Contact {
-  firstName: String;
-  lastName: String;
-  email: String;
+interface Post {
+  title: String;
+  day: String;
+  date: Date;
+  description: String;
+  image: String;
+  todayInOneSentence: String;
+  heading: String;
+  content: String;
 }
 
-// Add new contact
-app.post("/contacts", async (req, res) => {
+// Get a post by day
+app.get("/posts/:day", (req, res) => {
+  const day = req.params.day.toLowerCase()
+  const queryArray = [
+    ["day", "==", day]
+  ];
+
+  firebaseHelper.firestore
+    .queryData(db, postsCollection, queryArray)
+    .then((doc) => res.status(200).send(doc))
+    .catch((error) => res.status(400).send(`Cannot get post: ${error}`));
+});
+
+// Get all posts
+app.get("/posts", (req, res) => {
+  firebaseHelper.firestore
+    .backup(db, postsCollection)
+    .then((data) => res.status(200).send(data))
+    .catch((error) => res.status(400).send(`Cannot get posts: ${error}`));
+});
+
+// Add a new post
+app.post("/posts", async (req, res) => {
   try {
-    const contact: Contact = {
-      firstName: req.body["firstName"],
-      lastName: req.body["lastName"],
-      email: req.body["email"]
+    const post: Post = {
+      title: req.body["title"],
+      day: req.body["day"],
+      date: req.body["date"],
+      description: req.body["description"],
+      image: req.body["image"],
+      todayInOneSentence: req.body["todayInOneSentence"],
+      heading: req.body["heading"],
+      content: req.body["content"]
     };
 
-    const newDoc = await firebaseHelper.firestore.createNewDocument(
+    const newDoc = await firebaseHelper.firestore.createDocumentWithID(
       db,
-      contactsCollection,
-      contact
+      'posts',
+      req.body["day"],
+      post
     );
-    res.status(201).send(`Created a new contact: ${newDoc.id}`);
+    res.status(201).send(`Created a new post: ${newDoc}`);
   } catch (error) {
-    res
-      .status(400)
-      .send(`Contact should only contains firstName, lastName and email!!!`);
+    res.status(400).send(`Error: <br /><pre>${error}</pre>`);
   }
 });
 
-// Update new contact
-app.patch("/contacts/:contactId", async (req, res) => {
+// Update a post
+app.patch("/posts/:day", async (req, res) => {
   const updatedDoc = await firebaseHelper.firestore.updateDocument(
     db,
-    contactsCollection,
-    req.params.contactId,
+    postsCollection,
+    req.params.day,
     req.body
   );
-  res.status(204).send(`Update a new contact: ${updatedDoc}`);
+  res.status(204).send(`Updated post: ${updatedDoc}`);
 });
 
-// View a contact
-app.get("/contacts/:contactId", (req, res) => {
-  firebaseHelper.firestore
-    .getDocument(db, contactsCollection, req.params.contactId)
-    .then((doc) => res.status(200).send(doc))
-    .catch((error) => res.status(400).send(`Cannot get contact: ${error}`));
-});
-
-// View all contacts
-app.get("/contacts", (req, res) => {
-  firebaseHelper.firestore
-    .backup(db, contactsCollection)
-    .then((data) => res.status(200).send(data))
-    .catch((error) => res.status(400).send(`Cannot get contacts: ${error}`));
-});
-
-// Delete a contact
-app.delete("/contacts/:contactId", async (req, res) => {
-  const deletedContact = await firebaseHelper.firestore.deleteDocument(
+// Delete a post
+app.delete("/posts/:day", async (req, res) => {
+  const deletedPost = await firebaseHelper.firestore.deleteDocument(
     db,
-    contactsCollection,
-    req.params.contactId
+    postsCollection,
+    req.params.day
   );
-  res.status(204).send(`Contact is deleted: ${deletedContact}`);
+  res.status(204).send(`Post has been deleted deleted: ${deletedPost}`);
 });
 
 export { app };
